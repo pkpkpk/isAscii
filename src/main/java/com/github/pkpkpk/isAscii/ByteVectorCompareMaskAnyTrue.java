f95f2e82f473f774eea7499ddc36687d4f027f72
pat <pat.killean@gmail.com>
2023-05-23 06:00:22 -0400

    get byte index from ByteVectorCompareMask

--- src/main/java/com/github/pkpkpk/isAscii/ByteVectorCompareMaskAnyTrue.java
+++ src/main/java/com/github/pkpkpk/isAscii/ByteVectorCompareMaskAnyTrue.java
@@ -5,13 +5,19 @@ import jdk.incubator.vector.*;
 public class ByteVectorCompareMaskAnyTrue implements AsciiChecker {
 
     public int check(byte[] byteArray) {
-        ByteVector zero = ByteVector.zero(VectorSpecies.ofPreferred(byte.class));
+        VectorSpecies<Byte> SPECIES = ByteVector.SPECIES_PREFERRED;
+        ByteVector zero = ByteVector.zero(SPECIES);
         int i = 0;
 
         for (; i <= byteArray.length - zero.length(); i += zero.length()) {
-            ByteVector v = ByteVector.fromArray(zero.species(), byteArray, i);
-            if (v.compare(VectorOperators.UNSIGNED_GE, (byte) 0b10000000).anyTrue()) {
-                return i;
+            ByteVector v = ByteVector.fromArray(SPECIES, byteArray, i);
+            VectorMask<Byte> mask = v.compare(VectorOperators.UNSIGNED_GE, (byte) 0b10000000);
+            if (mask.anyTrue()) {
+                for (int j = 0; j < mask.length(); j++) {
+                    if (mask.laneIsSet(j)) {
+                        return i + j;
+                    }
+                }
             }
         }
 
--- src/main/java/com/github/pkpkpk/isAscii/ByteVectorCompareMaskToLong.java
+++ src/main/java/com/github/pkpkpk/isAscii/ByteVectorCompareMaskToLong.java
@@ -1,19 +1,26 @@
 package com.github.pkpkpk.isAscii;
 
 import jdk.incubator.vector.ByteVector;
+import jdk.incubator.vector.VectorMask;
 import jdk.incubator.vector.VectorOperators;
 import jdk.incubator.vector.VectorSpecies;
 
 public class ByteVectorCompareMaskToLong implements AsciiChecker {
 
     public int check(byte[] byteArray) {
-        ByteVector zero = ByteVector.zero(VectorSpecies.ofPreferred(byte.class));
+        VectorSpecies<Byte> SPECIES = ByteVector.SPECIES_PREFERRED;
+        ByteVector zero = ByteVector.zero(SPECIES);
         int i = 0;
 
         for (; i <= byteArray.length - zero.length(); i += zero.length()) {
-            ByteVector v = ByteVector.fromArray(zero.species(), byteArray, i);
-            if (v.compare(VectorOperators.UNSIGNED_GE, (byte) 0b10000000).toLong() != 0) {
-                return i;
+            ByteVector v = ByteVector.fromArray(SPECIES, byteArray, i);
+            VectorMask<Byte> mask = v.compare(VectorOperators.UNSIGNED_GE, (byte) 0b10000000);
+            if (mask.toLong() != 0L) {
+                for (int j = 0; j < SPECIES.length(); j++) {
+                    if (mask.laneIsSet(j)) {
+                        return i + j;
+                    }
+                }
             }
         }
 
