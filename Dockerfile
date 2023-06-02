f7c0c91a111f8a140cf610c4b8018161b88101f2
pat <pat.killean@gmail.com>
2023-06-01 21:41:29 -0400

    switch to throughput, use blackhole

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ WORKDIR /app
 COPY . .
 RUN mvn clean compile package dependency:copy-dependencies
 ENV CLASSPATH="/app/target/dependency/*"
-# without ea will DCE
-CMD ["/usr/lib/jvm/java-20-amazon-corretto/bin/java", "-ea", "-Xmx4g",  "--add-modules", "jdk.incubator.vector", "-cp", "target/benchmarks.jar", "org.openjdk.jmh.Main"]
+CMD ["/usr/lib/jvm/java-20-amazon-corretto/bin/java", "-Xmx4g",  "--add-modules", "jdk.incubator.vector", "-cp", "target/benchmarks.jar", "org.openjdk.jmh.Main"]
 
--- src/main/java/com/github/pkpkpk/isAscii/BenchmarkRunner.java
+++ src/main/java/com/github/pkpkpk/isAscii/BenchmarkRunner.java
@@ -1,11 +1,13 @@
 package com.github.pkpkpk.isAscii;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
 @Warmup(iterations = 3)
 @Measurement(iterations = 8)
 @Fork(1)
@@ -13,14 +15,17 @@ import java.util.Random;
 
 public class BenchmarkRunner {
     static final int N = 5;
+
     @State(Scope.Thread)
     public static class MyState {
         byte[] byteArray;
+        double sizeInGB;
 
         @Setup(Level.Trial)
         public void doSetup() {
             int size = Integer.MAX_VALUE - 8;
             byteArray = new byte[size];
+            sizeInGB = size / (double) (1 << 30);  // Convert bytes to GB
             Random random = new Random();
             for (int i = 0; i < size; i++) {
                 byteArray[i] = (byte) (random.nextInt('~' - ' ' + 1) + ' ');
@@ -29,27 +34,27 @@ public class BenchmarkRunner {
     }
 
     @Benchmark
-    public void benchScalar(MyState state) {
+    public void benchScalar(MyState state, Blackhole bh) {
         AsciiChecker checker = new Scalar();
-        assert -1 == checker.check(state.byteArray);
+        bh.consume(checker.check(state.byteArray));
     }
 
     @Benchmark
-    public void benchByteVectorCompareMaskAnyTrue(MyState state) {
+    public void benchByteVectorCompareMaskAnyTrue(MyState state, Blackhole bh) {
         AsciiChecker checker = new ByteVectorCompareMaskAnyTrue();
-        assert -1 == checker.check(state.byteArray);
+        bh.consume(checker.check(state.byteArray));
     }
 
     @Benchmark
-    public void benchByteVectorCompareMaskToLong(MyState state) {
+    public void benchByteVectorCompareMaskToLong(MyState state, Blackhole bh) {
         AsciiChecker checker = new ByteVectorCompareMaskToLong();
-        assert -1 == checker.check(state.byteArray);
+        bh.consume(checker.check(state.byteArray));
     }
 
     @Benchmark
-    public void benchByteVectorLaneReduction(MyState state) {
+    public void benchByteVectorLaneReduction(MyState state, Blackhole bh) {
         AsciiChecker checker = new ByteVectorLaneReduction();
-        assert -1 == checker.check(state.byteArray);
+        bh.consume(checker.check(state.byteArray));
     }
 
     public static void main(String[] args) throws Exception {
